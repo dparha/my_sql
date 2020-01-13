99421764543efea6951f4bde457780571c519d10
Daniel Parham <dparha012@gmail.com>
2020-01-12 22:42:29 -0600

    fixed table name

--- create_dogs.sql
+++ create_dogs.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE dogs;
 USE dogs;
-CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20),
+CREATE TABLE pets (name VARCHAR(20), owner VARCHAR(20),
                   species VARCHAR(20), sex CHAR(1),
                   birth DATE, death DATE);
